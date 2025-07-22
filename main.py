58689d07f6d868ca7d3d9544a4cd122fb261aa0f
perejilinformatico <perejilinformatico@gmail.com>
2025-07-22 01:09:10 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+import random 
+Cosas_Sobre_mi = ["Me gusta la musica","Me gusta la milanesa","Amo a los gatos","Mi color favorito es el rojo","Mi banda favorita es Queen"]
+print(random.choice(Cosas_Sobre_mi))
